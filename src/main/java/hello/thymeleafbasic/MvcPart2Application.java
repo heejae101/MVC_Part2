f591f8f738f5a32c5c6395d18683521f16486b65
heejae101 <heejae0629@naver.com>
2023-10-18 14:31:19 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleafbasic/MvcPart2Application.java
@@ -0,0 +1,13 @@
+package hello.thymeleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymeleafbasic/MvcPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
